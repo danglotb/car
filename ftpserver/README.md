ae143727973ec022ded161ccf5fcdbaa8c07e525
danglobt <bdanglot@gmail.com>
2015-01-29 12:32:12 +0100

    add readme, account and some prototype of process

---
+++ ftpserver/README.md
@@ -0,0 +1,7 @@
+# car
+workspace car
+
+directory is the file system used by the ftp server
+=========
+
+file : .account contains tuples (login:password) to build in map in the server.
--- ftpserver/src/serverftp/FtpRequest.java
+++ ftpserver/src/serverftp/FtpRequest.java
@@ -11,9 +11,20 @@ import java.util.Scanner;
 
 public class FtpRequest extends Thread {
 
+	/**
+	 * Socket server
+	 */
 	private Socket serv;
+	
+	/**
+	 * Boolean to loop on the processing
+	 */
 	private boolean end;
 
+	/**
+	 * Constructor
+	 * @param serv
+	 */
 	public FtpRequest(Socket serv) {
 		this.serv = serv;
 		String ready = "220 ready\n";
@@ -38,6 +49,10 @@ public class FtpRequest extends Thread {
 		}
 	}
 
+	/**
+	 * Method processing request
+	 * @throws IOException
+	 */
 	public void processRequest() throws IOException {
 		InputStream in = this.serv.getInputStream();
 		BufferedReader bf = new BufferedReader(new InputStreamReader(in));
@@ -50,6 +65,10 @@ public class FtpRequest extends Thread {
 		switch (type) {
 		case "USER":
 			rep = processUser(sc.next());
+		case "PASS":
+			rep = processPass(sc.next());
+		case "QUIT":
+			rep = processQuit();
 		default:
 			
 		}
@@ -64,12 +83,31 @@ public class FtpRequest extends Thread {
 
 	/**
 	 * method processing USER request
-	 * 
+	 * @param req : username send by the user
 	 * @return response for a USER request
 	 */
 	public String processUser(String req) {
 		return "331 User name okay, need password.\n";
 	}
-	/* @TODO other process */
+	
+	/**
+	 * method processing PASS request
+	 * @param req : password send by the user
+	 * @return response for a PASS request
+	 */
+	public String processPass(String req) {
+		return "";
+	}
+	
+	/**
+	 * method processing QUIT request
+	 * set the boolean end to true to end the service
+	 * @return response for a QUIT request
+	 */
+	public String processQuit() {
+		this.end = true;
+		return "";
+	}
+	/* TODO other process */
 
 }
