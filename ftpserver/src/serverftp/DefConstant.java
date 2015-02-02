fb8f8e71d722949e26085dade5da67a1fa44f0cb
Mandrivia <antoine.philippe62@gmail.com>
2015-02-02 15:13:55 +0100

    fixed break bug + implemtation of LIST and  SYST (not working)

--- ftpserver/src/serverftp/DefConstant.java
+++ ftpserver/src/serverftp/DefConstant.java
@@ -4,10 +4,13 @@ public class DefConstant {
 	
 	/* file where is stored tuples of login / pass */
 	public static final String ACCOUNT_FILE = ".account";
+	
+	public static final String CURRENT_DIR = ".";
 
 	/* list of type of request */
 	public static final String USER = "USER";
 	public static final String PASS = "PASS";
+	public static final String SYST = "SYST";
 	public static final String RETR = "RETR";
 	public static final String STOR = "STOR";
 	public static final String LIST = "LIST";
@@ -20,8 +23,9 @@ public class DefConstant {
 	
 	public static final String READY = "220 ready\n";
 	public static final String GOOD_USER = "331 User name okay, need password.\n";
-	public static final String GOOD_PASS =  "230	User logged in, proceed. Logged out if appropriate.\n";
+	public static final String GOOD_PASS =  "230 User logged in, proceed. Logged out if appropriate.\n";
 	public static final String NEED_USER = "332 Need account for login.\n";
+	public static final String SYST_INFO = "215 LINUX";
 	
 	public static final String WRONG_USER_OR_PASS = "430 Invalid username or password.\n";
 	
--- ftpserver/src/serverftp/FtpRequest.java
+++ ftpserver/src/serverftp/FtpRequest.java
@@ -2,11 +2,14 @@ package serverftp;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class FtpRequest extends Thread {
@@ -45,7 +48,7 @@ public class FtpRequest extends Thread {
 	}
 
 	/**
-	 * main method wich is call processRequest while the end of service
+	 * main method which is calling processRequest while the end of service
 	 */
 	public void run() {
 		while (!end) {
@@ -71,13 +74,23 @@ public class FtpRequest extends Thread {
 		String rep = "";
 		/* switching on the type of the request */
 		switch (type) {
-		case DefConstant.USER:
-			rep = processUser(sc.next());
-		case DefConstant.PASS:
-			rep = processPass(sc.next());
-		case DefConstant.QUIT:
-			rep = processQuit();
-		default:
+			case DefConstant.USER:
+				rep = processUser(sc.next());
+				break;
+			case DefConstant.PASS:
+				rep = processPass(sc.next());
+				break;
+			case DefConstant.LIST:
+				rep = processList();
+				break;
+			case DefConstant.SYST:
+				System.out.println(DefConstant.SYST_INFO);
+				rep = DefConstant.SYST_INFO;
+				break;
+			case DefConstant.QUIT:
+				rep = processQuit();
+				break;
+			default:
 			
 		}
 		/* send the response */
@@ -95,6 +108,7 @@ public class FtpRequest extends Thread {
 	 * @return response for a USER request
 	 */
 	public String processUser(String req) {
+		System.out.println("user");
 		if (Server.userExist(req)) {
 			this.user = req;
 			return DefConstant.GOOD_USER;
@@ -108,12 +122,39 @@ public class FtpRequest extends Thread {
 	 * @return response for a PASS request
 	 */
 	public String processPass(String req) {
+		System.out.println("mdp");
 		if (this.user.equals(""))
 			return DefConstant.NEED_USER;
-		if (Server.getPass(this.user, req))
+		if (Server.getPass(this.user, req)){ 
+			System.out.println(DefConstant.GOOD_PASS);
 			return DefConstant.GOOD_PASS;
-		else
+			
+		} else
 			return DefConstant.WRONG_USER_OR_PASS;
+	}
+	
+	/**
+	 * method processing RETR request
+	 * @param fileName : file to be sent on the server (
+	 */
+	public void processRetr(String fileName){
+		
+		
+	}
+	
+	/**
+	 *  method processing LIST request
+	 *  @return fileList : list of files of current directory
+	 */
+	/* TODO: implement correct return codes */
+	public String processList(){
+		File directory = new File(DefConstant.CURRENT_DIR);
+		File[] files = directory.listFiles();
+		List<String> fileList = new ArrayList<String>();
+		for(File file : files){
+			fileList.add(file.toString());
+		}
+			return "350 request";
 	}
 	
 	/**
