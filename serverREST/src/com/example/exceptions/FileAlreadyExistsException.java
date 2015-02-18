5794c60ae431e78a24ac462885443ff203387116
danglotb <bdanglot@gmail.com>
2015-02-18 12:10:48 +0100

    add getFiles() to get the list of file (NLIST)

--- serverREST/src/com/example/exceptions/FileAlreadyExistsException.java
+++ serverREST/src/com/example/exceptions/FileAlreadyExistsException.java
@@ -1,7 +1,10 @@
 package com.example.exceptions;
 
-public class FileAlreadyExistsException extends Exception {
-	
+import javax.ws.rs.WebApplicationException;
+
+public class FileAlreadyExistsException extends  WebApplicationException {
+	private static final long serialVersionUID = 0564600154541L;
+
 	public FileAlreadyExistsException(String filePath){
 		System.err.println("File " + filePath + " already exists !");
 	}
