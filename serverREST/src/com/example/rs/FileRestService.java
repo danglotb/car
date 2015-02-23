86fd5dcb6c79a36ee6a1a29ff23f31ed35a2dcaf
Mandrivia <antoine.philippe62@gmail.com>
2015-02-23 12:28:03 +0100

    tried to make first test, not working

--- serverREST/src/com/example/rs/FileRestService.java
+++ serverREST/src/com/example/rs/FileRestService.java
@@ -1,11 +1,11 @@
 package com.example.rs;
 
 import java.util.Collection;
-import java.util.List;
 
 import javax.inject.Inject;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -17,21 +17,27 @@ import com.example.services.FileService;
 @Path("/file")
 public class FileRestService {
 
-	@Inject private FileService fileService;
-	
-	@Produces( { MediaType.APPLICATION_JSON } )
-	@GET
-	public Collection<File> getFiles(@QueryParam( "page") @DefaultValue( "1" ) final int page ) {
-		return fileService.getFile(page, 5);
-	}
-	
-	
-	@Produces( { MediaType.APPLICATION_JSON } )
-	@Path ("/{filePath}")
-	@GET
-	public File getFile(@QueryParam( "filePath") final String filePath) {
+	@Inject
+	private FileService fileService;
+
+	@POST
+	public File addFile(String name, String filePath){
+		fileService.addFile(filePath, name);
 		return fileService.getByPath(filePath);
 	}
 	
-	
+	@Produces({ MediaType.APPLICATION_JSON })
+	@GET
+	public Collection<File> getFiles(
+			@QueryParam("page") @DefaultValue("1") final int page) {
+		return fileService.getFile(page, 5);
+	}
+
+	@Produces({ MediaType.APPLICATION_JSON })
+	@Path("/{filePath}")
+	@GET
+	public File getFile(@QueryParam("filePath") final String filePath) {
+		return fileService.getByPath(filePath);
+	}
+
 }
