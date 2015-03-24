2cb45d5da4f8ba8ec93cb4098798159d800b257f
danglobt <bdanglot@gmail.com>
2015-03-24 13:38:43 +0100

    add test for addFile (doesn't work)

--- serverREST/src/rest/tests/RESTTest.java
+++ serverREST/src/rest/tests/RESTTest.java
@@ -1,11 +1,14 @@
 package rest.tests;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -13,27 +16,27 @@ import org.junit.Test;
 /**
  * Class general de Test pour la passerelle REST
  * 
- * \/ ! \\ verifiez que le serveur FTP est egalement lance (sur le port 9874 pour le moment...)
+ * \/ ! \\ verifiez que le serveur FTP est egalement lance (sur le port 9874
+ * pour le moment...)
  * 
  */
 public class RESTTest {
-	
+
 	/**
 	 * StarterTest pour lancer la passerelle REST dans un autre Thread
 	 */
 	private static StarterTest st;
-	
-	
+
 	/**
-	 * Initialise les differentes classes utiles aux tests.
-	 * Lance notamment la passerelle REST dans un autre Thread grace au StarterTest
+	 * Initialise les differentes classes utiles aux tests. Lance notamment la
+	 * passerelle REST dans un autre Thread grace au StarterTest
 	 */
 	@BeforeClass
 	public static void setUp() {
 		st = new StarterTest();
-		st.run();
+	//	st.run();
 	}
-	
+
 	/**
 	 * Test la fonction getFileList() en envoyant une requete GET à l'url :
 	 * http://127.0.0.1:8080/rest/api/file/
@@ -46,10 +49,11 @@ public class RESTTest {
 		try {
 			url = new URL(urlStr);
 			URLConnection uConnection = url.openConnection();
-			BufferedReader in = new BufferedReader(new InputStreamReader(uConnection.getInputStream()));
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					uConnection.getInputStream()));
 			String inputLine;
 			while ((inputLine = in.readLine()) != null)
-				ret +=inputLine;
+				ret += inputLine;
 			System.out.println(ret);
 			in.close();
 		} catch (MalformedURLException e) {
@@ -58,13 +62,43 @@ public class RESTTest {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Test la methode d'envoye d'un fichier avec une requete POST
 	 */
 	@Test
 	public void addFileTest() {
-		
+		String strURL = "http://127.0.0.1:8080/rest/api/file/";
+		String content, tmp;
+		OutputStreamWriter writer = null;
+		BufferedReader reader = null;
+		try {
+			
+			reader = new BufferedReader(new InputStreamReader(new FileInputStream("testAddFile")));
+
+			content="content=";
+			
+			/* c'est cool l'utf-8 */
+			while ((tmp = reader.readLine()) != null)
+				content+= URLEncoder.encode(tmp,"UTF-8");
+			
+			content+="&amp;name="+URLEncoder.encode("testAddFile","UTF-8");
+			
+			URL url = new URL(strURL);
+			URLConnection uConnection = url.openConnection();
+			uConnection.setDoOutput(true);
+			
+			writer = new OutputStreamWriter(uConnection.getOutputStream());
+			writer.write(content);
+			writer.flush();
+			
+			writer.close();
+			
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 }
