b0ab5f8d77d002c1afc7bc1f3df52f53b843fb8c
danglotb <bdanglot@gmail.com>
2015-03-24 18:52:28 +0100

    merge

--- serverREST/src/rest/rs/FileRestService.java
+++ serverREST/src/rest/rs/FileRestService.java
@@ -22,7 +22,6 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.UriInfo;
 
-
 import org.apache.cxf.jaxrs.ext.multipart.Multipart;
 
 import rest.services.FileService;
--- serverREST/src/rest/services/FileService.java
+++ serverREST/src/rest/services/FileService.java
@@ -1,17 +1,8 @@
 package rest.services;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
 
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
