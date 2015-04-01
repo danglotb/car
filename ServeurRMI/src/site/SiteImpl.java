47b7f0f929c20a48f9337fcf54ff447e66fa49ab
Mandrivia <antoine.philippe62@gmail.com>
2015-04-01 11:22:14 +0200

    added Skel + annuaire

---
+++ ServeurRMI/src/site/SiteImpl.java
@@ -0,0 +1,53 @@
+package site;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class SiteImpl extends UnicastRemoteObject implements SiteItf {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private List<SiteItf> nodes;
+	private static int num = 1;
+	private int monNum;
+	public boolean propage;
+	
+	protected SiteImpl() throws RemoteException {
+		this.monNum = SiteImpl.num++;
+		this.nodes = new ArrayList<SiteItf>();
+		this.propage = false;
+	}
+
+	public void spread(final byte [] data) throws RemoteException {
+		// Propage les donnees a tous ses fils
+		if (propage) return;
+		System.out.println(" Noeud n° " +  this.monNum + " : données reçues, je propage à mes fils");
+		for (final SiteItf node : this.nodes) {
+				new Thread () {
+					public void run() {
+						try {
+							node.spread(data);
+						} catch (RemoteException e) {e.printStackTrace();}
+					}
+				}.start();
+		}
+		System.out.println(this.monNum + "données propagées");
+		propage = true;
+	}
+
+	public List<SiteItf> getFils() throws RemoteException {
+		return nodes;
+	}
+
+	public void setNodes(List<SiteItf> nodes) throws RemoteException{
+		this.nodes = nodes;
+	}
+	
+	public void addNode(SiteItf node) throws RemoteException{
+		this.nodes.add(node);
+	}
+}
---
+++ ServeurRMI/src/site/SiteItf.java
@@ -0,0 +1,11 @@
+package site;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+
+public interface SiteItf extends Remote {
+	
+	public void addNode(SiteItf node)  throws RemoteException;
+	public void spread(byte [] data) throws RemoteException;
+
+}
