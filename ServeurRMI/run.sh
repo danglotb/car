a86d0d3a60cd1a48e977775b9ce6e7f92590a10b
danglotb <bdanglot@gmail.com>
2015-04-07 15:50:09 +0200

    add some stuff in README

--- ServeurRMI/run.sh
+++ ServeurRMI/run.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
 #check args
-if [ $# -eq 0 ] ; then
-    echo 'Need a file to build the Data Structure'
+if [ $# -lt 1 ] ; then
+    echo 'Need a file to build the Data Structure : usage : ./run.sh (-g) <struct_file>'
     exit 1
 fi
 
 path=rmi.executables
-file=$1
-index=1
 
-#cd bin
+if [ $# -eq 1 ] ; then
+	file=$1
+else
+	file=$2
+fi
+
+index=1
 
 #launch nodes
 echo 'launching nodes...'
 while read p
 do
     echo $index $p
-    xterm -e java -jar LaunchNodeTree.jar $index $p &
+ 	if [ $1 == "-g" ] ; then
+ 		xterm -e java -jar LaunchNodeGraph.jar $index $p &
+    else
+    	xterm -e java -jar LaunchNodeTree.jar $index $p &
+    fi
     index=$(($index+1))
 done < $file
-
-#cd ..
